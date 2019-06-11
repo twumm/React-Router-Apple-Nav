b495ed6fe3f6ab57ecf7ddd4fb58547654ad109b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-11 12:13:14 +0000

    feature: Create and style nav items Map through nav data, rendering all main links onto the screen

--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
+import navLinksData from '../navLinkData';
+
+const StyledNavWrapper = styled.div`
+  background-color: #333333;
+  display: flex;
+  justify-content: space-between;
+  color: white;
+  align-items: center;
+  line-height: 3;
+`;
+
+const StyledLeftNavLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 8%;
+`;
+
+const StyledCenterNavLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+`;
+
+const StyledRightNavLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 16%;
+`;
 
 export default function NavWrapper() {
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  )
+    <StyledNavWrapper>
+      <StyledLeftNavLinks>
+        <img alt="Logo" />
+      </StyledLeftNavLinks>
+
+      <StyledCenterNavLinks>
+        {
+          navLinksData.map(navLink => (
+            <div key={navLink.id}>
+              {navLink.title}
+            </div>
+          ))
+        }
+      </StyledCenterNavLinks>
+
+      <StyledRightNavLinks>
+        <div>
+          <img alt="Search" />
+        </div>
+        <div>
+          <img alt="Shop" />
+        </div>
+      </StyledRightNavLinks>
+    </StyledNavWrapper>
+  );
 }
